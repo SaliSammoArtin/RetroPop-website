e3cb0867717a74d8c2130bab00c30e10f00db441
SaliSammoArtin <Salisammo@live.se>
2026-09-10 08:39:22 +0200

    Fetch products on inventory page

--- src/inventory/components/InventoryPage.jsx
+++ src/inventory/components/InventoryPage.jsx
@@ -1,5 +1,25 @@
+import { useEffect, useState } from "react";
+
 //Skapar komponenten
 function InventoryPage() {
+  const [products, setProducts] = useState([]);
+
+  async function getProducts() {
+    const response = await fetch("http://localhost:3000/products");
+    const result = await response.json();
+
+    if (response.ok) {
+      //sparar produkterna i state
+      setProducts(result);
+    } else {
+      console.log("Error!");
+    }
+  }
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return (
     <section>
       <h1>Inventory</h1>
